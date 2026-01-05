caa369831ae524fb864775eed295184eb1a46e7d
suteetoe <sutee.toe@gmail.com>
2026-01-05 10:07:29 +0700

    check ai barcode type input for extract serial and product code

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,10 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+    },
   },
   server: {
     port: 8080
