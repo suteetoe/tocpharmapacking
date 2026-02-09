05df53b737d188566d6e83ddd1639d2a01e3e361
suteetoe <sutee.toe@gmail.com>
2026-02-09 20:19:36 +0700

    fix:: fix vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,39 +1,29 @@
 /// <reference types="vitest" />
 import { fileURLToPath, URL } from "node:url";
-import { defineConfig, loadEnv } from "vite";
+import { defineConfig } from "vite";
 
 // import { defineConfig } from 'vitest/config'
 import vue from "@vitejs/plugin-vue";
 import VueDevTools from "vite-plugin-vue-devtools";
 
 // https://vite.dev/config/
-export default defineConfig(({ mode }) => {
-
-  const env = loadEnv(mode, process.cwd(), "");
-
-
-  const baseUrl = env.VITE_APP_BASE_URL || "/";
-
-  console.log("VITE APP BASE URL:", baseUrl);
-  
-  return {
-    plugins: [vue(), VueDevTools()],
-    // test: {
-    //   globals: true,
-    //   environment: 'jsdom',
-    //   coverage: {
-    //     provider: 'v8',
-    //     reporter: ['text', 'json', 'html'],
-    //   },
-    // },
-    resolve: {
-      alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
-      },
+export default defineConfig({
+  plugins: [vue(), VueDevTools()],
+  // test: {
+  //   globals: true,
+  //   environment: 'jsdom',
+  //   coverage: {
+  //     provider: 'v8',
+  //     reporter: ['text', 'json', 'html'],
+  //   },
+  // },
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
-    base: env.VITE_APP_BASE_URL || "/",
-    server: {
-      port: 8080,
-    },
-  };
+  },
+  base: process.env.VITE_APP_BASE_URL || "/",
+  server: {
+    port: 8080,
+  },
 });
