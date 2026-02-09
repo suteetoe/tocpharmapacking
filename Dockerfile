e1c0b4ee6e4c9844b3763ac21439f64af776ae72
suteetoe <sutee.toe@gmail.com>
2026-02-09 15:50:37 +0700

    feat:: replace index for base url

--- Dockerfile
+++ Dockerfile
@@ -12,6 +12,10 @@ WORKDIR /app
 RUN npm install -g serve
 COPY --from=build-stage /app/dist ./dist
 
+# replace  href="/ in index.html to VITE_APP_BASE_URL_PLACEHOLDER for support base url
+RUN sed -i 's|href="/|href="VITE_APP_BASE_URL_PLACEHOLDER|g' ./dist/index.html
+RUN sed -i 's|src="/|src="VITE_APP_BASE_URL_PLACEHOLDER|g' ./dist/index.html
+
 # Copy entrypoint script เข้าไป
 COPY entrypoint.sh /entrypoint.sh
 RUN chmod +x /entrypoint.sh
--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,22 @@
 # ตำแหน่งไฟล์ config ในโฟลเดอร์ dist
 CONFIG_FILE="/app/dist/config.js"
 
+INDEX_FILE="/app/dist/index.html"
+
 echo "Applying Runtime Config..."
 
+if [ -z "$VITE_APP_BASE_URL" ]; then
+  VITE_APP_BASE_URL="/"
+fi
 # แทนที่ค่าในไฟล์ config.js ด้วย Environment Variables
 # ตัวอย่าง: เปลี่ยน placeholder เป็นค่าจาก docker -e
 sed -i "s|VITE_APP_NAME_PLACEHOLDER|${VITE_APP_NAME}|g" $CONFIG_FILE
 sed -i "s|VITE_APP_BASE_URL_PLACEHOLDER|${VITE_APP_BASE_URL}|g" $CONFIG_FILE
 sed -i "s|VITE_API_URL_PLACEHOLDER|${VITE_API_URL}|g" $CONFIG_FILE
+
+# แทนที่ src ของ config.js ใน index.html ด้วย VITE_APP_BASE_URL
+sed -i "s|src=\"/config.js\"|src=\"${VITE_APP_BASE_URL}config.js\"|g" $INDEX_FILE
+sed -i "s|src=\"/assets/|src=\"${VITE_APP_BASE_URL}assets/|g" $INDEX_FILE
 
 echo "Starting serve..."
 
